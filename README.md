c1e1e9c83deede0462609c75279171e1f46f9b07
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2026-05-20 00:43:45 -0400

    Create a basic window with SDL3

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # OctoCore
 
 A Chip-8 emulator written in C
+
+## Dependencies
+* SDL3
+
+## Building
+
+### Mac
+* Ensure SDL3 is installed
+* Build with `gcc -c src/main.c $CFLAGS`
+* Link with `gcc main.o $LDFLAGS -lSDL3`
+* Run with `./main`
+
---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include "SDL3/SDL.h"
+
+int main(int argc, char *argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow("SDL3", 640, 480, 0);
+
+    int quit = 0;
+    while (!quit) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_EVENT_QUIT) {
+                quit = true;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
+
