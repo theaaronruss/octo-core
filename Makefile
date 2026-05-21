9e58eac528518a735be96f5e30dbcc8a24e6c0e1
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2026-05-21 15:44:23 -0400

    Add basic CPU representation

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 CC := gcc
 
-octocore: main.o
+octocore: main.o cpu.o
 	$(CC) $^ $(LDFLAGS) -lSDL3 -o octocore
 
 main.o: src/main.c
 	$(CC) $(CFLAGS) -c src/main.c
+
+cpu.o: src/cpu.c
+	$(CC) $(CFLAGS) -I./includes -c src/cpu.c
 
 .PHONY: clean
 clean:
---
+++ includes/cpu.h
@@ -0,0 +1,39 @@
+#ifndef CPU_H
+#define CPU_H
+
+#include <stdint.h>
+
+struct cpu {
+    // Registers
+    int8_t v0;
+    int8_t v1;
+    int8_t v2;
+    int8_t v3;
+    int8_t v4;
+    int8_t v5;
+    int8_t v6;
+    int8_t v7;
+    int8_t v8;
+    int8_t v9;
+    int8_t vA;
+    int8_t vB;
+    int8_t vC;
+    int8_t vD;
+    int8_t vE;
+    int8_t vF;
+    int16_t i;
+    int8_t dt;
+    int8_t st;
+
+    // Program counter
+    int16_t pc;
+
+    // Stack pointer
+    int8_t sp;
+
+    // Stack
+    int16_t stack[];
+};
+
+#endif
+
